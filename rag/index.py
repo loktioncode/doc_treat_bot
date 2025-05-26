4d85190e501064f305efc55ab37fbab8cb76bd23
elishabere <>
2025-05-27 00:04:30 +0200

    update index rag

--- rag/index.py
+++ rag/index.py
@@ -1,123 +1,197 @@
-# -*- coding: utf-8 -*-
-import uvicorn
+import json
+import os
+from dotenv import load_dotenv
+import io
 from PIL import Image
-import numpy as np
-import time
-from fastapi import FastAPI, Request
+from langsmith import traceable
 import requests
+import time
+from langchain_community.vectorstores import FAISS
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI, HarmBlockThreshold, \
+    HarmCategory
+import google.generativeai as genai
+from exa_py import Exa
+from langchain.agents import tool, AgentExecutor, create_react_agent
+from langchain.tools.retriever import create_retriever_tool
 
-from rag.index import describe_media
-from respond import twilio_account_sid, twilio_auth_token, chat_with_phil, send_sms
-import tensorflow as tf
-from io import BytesIO
-from twilio.twiml.messaging_response import MessagingResponse
+from rag.prompt import prompt
+from utils.utils import format_response
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 
+import json
 
-# Keep the application running to listen for updates
-# Check TensorFlow version
-print(tf.__version__)  # This should print 2.15.0
-app = FastAPI()
+load_dotenv()
 
+# Initialize Firebase Admin SDK
+# cred_obj = credentials.Certificate('./rag/config.json')
 
-#CUSTOM AI MODELS
-CLASS_NAMES = ['Tomato_Bacterial_spot',
-               'Tomato_Early_blight',
-               'Tomato_Late_blight',
-               'Tomato_Leaf_Mold',
-               'Tomato_Septoria_leaf_spot',
-               'Tomato_Spider_mites_Two_spotted_spider_mite',
-               'Tomato__Target_Spot',
-               'Tomato__Tomato_YellowLeaf__Curl_Virus',
-               'Tomato__Tomato_mosaic_virus',
-               'Tomato_healthy']
+# default_app = firebase_admin.initialize_app(cred_obj, {
+#     'databaseURL': 'https://rusero-2cbc0-default-rtdb.europe-west1.firebasedatabase.app',
+#     'databaseAuthVariableOverride': None
+# })
+# client = firestore.client(app=default_app)
 
-corn_CLASS_NAMES = ['Corn Blight', 'Corn Common_Rust', 'Corn Gray_Leaf_Spot', 'Healthy Corn']
-potato_CLASS_NAMES = ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+exa = Exa(api_key=os.environ["EXA_KEY"])
 
-MODEL = tf.keras.models.load_model("models/tomato_model_v2.keras")
-potato_MODEL = tf.keras.models.load_model("models/potato.keras")
-corn_MODEL = tf.keras.models.load_model("models/corn_model_v1.keras")
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+new_db = FAISS.load_local("./rag/faiss_index", embeddings,
+                          allow_dangerous_deserialization=True)
 
+# Get the prompt to use
 
+# "models/gemini-1.5-flash"
+# "gemini-1.5-pro-latest"
+llm_model = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest", temperature=0, safety_settings={
+    HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+})
 
-
-@app.get('/')
-def index():
-    return {'message': 'bot is up'}
-
-
-def read_file_as_image(data) -> np.ndarray:
-    # Use Pillow to convert the binary data to a NumPy array representing an image
-    image = Image.open(BytesIO(data)).convert('RGB')
-
-    # Resize the image to the expected input size of the model
-    expected_size = (256, 256)  # Update with the actual input size expected by your model
-    image = image.resize(expected_size)
-
-    return np.array(image)
-
-def predict_disease(model, class_names, img_batch, user_phone_number):
-    """
-    Generic function to predict disease from image batch using specified model and class names.
-    
-    Args:
-        model: The ML model to use for prediction
-        class_names: List of class names for the model
-        img_batch: Batch of images to predict on
-        user_phone_number: Phone number to send results to
-    
-    Returns:
-        str: Formatted prediction result
-    """
-    predictions = model.predict(img_batch)
-    predicted_class = class_names[np.argmax(predictions[0])]
-    confidence = float(np.max(predictions[0]))
-    result = f"there is a chance of {predicted_class} with confidence, {confidence} from the patient images shared by the doctor"
-    chat_with_phil(result, to=f"whatsapp:+{user_phone_number}")
-    return result
+# doc retriever tool
+retriever = new_db.as_retriever(search_kwargs={'k': 1})
+retriever_tool = create_retriever_tool(
+    retriever,
+    "retriever",
+    "Query a retriever to get information about medical conditions, symptoms, treatments, and healthcare guidelines. "
+    "Help analyze medical symptoms and provide evidence-based recommendations for patient care."
+)
 
 
-@app.post('/gqirha')
-async def whatsapp_webhook(request: Request):
 
-    form_data = await request.form()
-    isifo = form_data.get("Body", "").strip().lower()
-    image_url = form_data.get("MediaUrl0", "")
-    user_phone_number = form_data.get('WaId', "")
-    # if not image_url:
-    #     return chat_with_phil(str("Please send an image of the isifo for prediction."))
-    print(form_data)
 
-    if isifo and not image_url:
-        # send user question to llm
-        return chat_with_phil(isifo, to=f"whatsapp:+{user_phone_number}")
 
-    response = requests.get(image_url, auth=(twilio_account_sid, twilio_auth_token))
+@tool
+def search(query: str):
+    """Search for a webpage based on the query."""
+    return exa.search_and_contents(f"{query}", use_autoprompt=True, num_results=5, type="auto", text=True)
 
-    if response.status_code != 200:
-        return str(MessagingResponse().message("Failed to download the image. Please try again."))
 
-    data = response.content
+# class AddReminderSchema(BaseModel):
+#     phone_number: str
+#     message: str
+#     remind_at: datetime
 
-    input_image = read_file_as_image(data)
-    img_batch = np.expand_dims(input_image, axis=0).astype(np.float32)
+# def get_patient_data(path) -> dict:
+#     # Get the data from the specified path
+#     snapshot = db.reference(path).get()
 
-    if isifo == '':
-        return chat_with_phil(describe_media(data), to=f"whatsapp:+{user_phone_number}", describe=True)
+#     # Create a new dictionary to store the data
+#     patient_data = {}
 
-#WE CAN ADD CUSTOM ML MODELS HERE
-    elif isifo == "potato":
-        predict_disease(potato_MODEL, potato_CLASS_NAMES, img_batch, user_phone_number)
-    elif isifo == "corn":
-        predict_disease(corn_MODEL, corn_CLASS_NAMES, img_batch, user_phone_number)
-    elif isifo == "tomato":
-        predict_disease(MODEL, CLASS_NAMES, img_batch, user_phone_number)
+#     # Iterate through the snapshot items and add them to the patient_data dictionary
+#     for key, val in snapshot.items():
+#         farm_data[key] = val
+
+#     # Return the farm_data dictionary
+#     return patient_data
+
+
+
+def read_file_as_image(data):
+    # If data is a file path, open and read the image directly
+    if isinstance(data, str):
+        return Image.open(data)
+    # If data is binary data, use io.BytesIO to read it
+    elif isinstance(data, bytes):
+        return Image.open(io.BytesIO(data))
     else:
-        chat_with_phil(isifo, to=f"whatsapp:+{user_phone_number}")
+        raise ValueError("Unsupported data format for image reading")
 
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8001)
+def describe_media(data):
+    """Analyzes medical images supplied by users for medical diagnosis support"""
+    system_instruction = (
+        "You are a medical assistant called MedAssist. A user has uploaded a medical image. Your task is to "
+        "analyze the image and provide preliminary medical insights. Identify any visible medical conditions, "
+        "abnormalities, or areas of concern. Provide detailed context about the medical image while maintaining "
+        "professional medical terminology. If you notice any concerning features, explain them clearly and suggest "
+        "next steps for medical evaluation. Remember to emphasize that this is a preliminary analysis and should "
+        "be followed up with professional medical consultation. Summarize responses to 1600 characters or less.")
 
-#uvicorn app:app --reloadurn {"item_id": item_id, "q": q}
+    print("Making LLM inference request...")
 
+    # Read the image from the input data
+    input_image = read_file_as_image(data)
+
+    # Save the image to a temporary file for uploading
+    temp_image_path = "temp_image.jpg"
+    input_image.save(temp_image_path)
+
+    media_file = genai.upload_file(path=temp_image_path)
+    print(f"Completed upload: {media_file.uri}")
+
+    while media_file.state.name == "PROCESSING":
+        print('Waiting for media to be processed.')
+        time.sleep(10)
+        media_file = genai.get_file(media_file.name)
+
+    if media_file.state.name == "FAILED":
+        raise ValueError(media_file.state.name)
+    print(f'media processing complete: ' + media_file.uri)
+
+    # Set the model to Gemini 1.5 Flash.
+    model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest")
+
+    response = model.generate_content([system_instruction, media_file],
+                                      request_options={"timeout": 600})
+
+    print(response.text)
+    return response.text
+
+
+#API CALL TO GET WEATHER DATA OR ANY DATA OF CHOICE
+@tool
+def fetch_weather(city):
+    """
+    Fetches weather data from  API to get city weather information.
+    """
+
+    # Base URL with placeholders for API key, latitude, and longitude
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&APPID={os.getenv('WEATHER_KEY')}&units=metric"
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for unsuccessful requests
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching weather data: {e}")
+        return None
+
+
+# setup tools and agent
+message_history = ChatMessageHistory()
+
+tools = [retriever_tool, search, fetch_weather]
+
+
+@traceable
+def user_input(user_question, to):
+
+
+    agent = create_react_agent(llm_model, tools, prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True)
+
+    # active_farmer = get_user_by_phone_number(to.replace("whatsapp:", ""))
+
+
+    agent_with_chat_history = RunnableWithMessageHistory(
+        agent_executor,
+        # This is needed because in most real world scenarios, a session id is needed
+        # It isn't really used here because we are using a simple in memory ChatMessageHistory
+        lambda session_id: message_history,
+        input_messages_key="input",
+        history_messages_key="chat_history",
+    )
+
+    response = agent_with_chat_history.invoke(
+        {
+            "input": ""}
+
+        ,
+        # This is needed because in most real world scenarios, a session id is needed
+        # It isn't really used here because we are using a simple in memory ChatMessageHistory
+        config={"configurable": {"session_id": 1234567890}},
+    )
+    print(response)
+    formatted_response = format_response(response)
+    return formatted_response
